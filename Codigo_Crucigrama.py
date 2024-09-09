70293028b4571862a1ddfde011b1b81e9a59e839
matiterra <matiasterranova1775@gmail.com>
2024-09-09 14:20:06 -0300

    Funcion de IngresarPalabraNumero

--- Codigo_Crucigrama.py
+++ Codigo_Crucigrama.py
@@ -16,3 +16,40 @@ def CargarListas(palabras_data):
     for _ in palabras_data:
         palabras.append(list(_['word']))
         definiciones.append(_['definition'])
+
+
+
+#Función encargada del Ingreso de la palabra a adivinar siguiendo la lógica de número - palabra (1 - C A S A)
+#Devuelve la palabra Ingresada, el número de la palabra a adivinar y si el usuario necesita una pista.
+def IngresarPalabraNumero():
+    bandera = True
+    while bandera:
+        try:
+            SeleccionaNumero = int(input("Ingrese el número de la palabra que quiere adivinar: "))
+            if 1 <= SeleccionaNumero <= 10:
+                bandera = False
+            else:
+                print("El número ingresado debe corresponder a uno de los números que se muestran en el tablero.")
+        except ValueError:
+            print("Por favor ingrese un número. Vuelva a intentarlo.")
+
+    bandera2 = True
+    while bandera2:
+        PedirPista = input("¿Desea pedir una pista extra? S = Sí / N = No: ").strip().upper()
+        if PedirPista == "S" or PedirPista == "N":
+            bandera2 = False
+        else:
+            print("Por favor, ingrese 'S' para Sí o 'N'  para No.")
+
+    bandera3 = True
+    while bandera3:
+        IngresaPalabra = input("Ingrese la palabra que quiere adivinar: ")
+        if IngresaPalabra.isalpha():
+            bandera3= False
+            return IngresaPalabra, SeleccionaNumero, PedirPista
+        else: 
+            print("Por favor ingrese una palabra. Vuelva a intentarlo.")
+
+
+#MAIN
+IngresarPalabraNumero()
