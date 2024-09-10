3f199883a06f5654d6d1594097c4cad80bd0a05e
lucianoperrella <lucianoperrella@gmail.com>
2024-09-10 15:24:17 -0300

    Actualizacion

--- Codigo_Crucigrama.py
+++ Codigo_Crucigrama.py
@@ -5,10 +5,21 @@ import json
 #Función encargada de leer json desde el archivo plano
 
 
+def LeerJSON():
+    lista_json = []
+    with open("palabras.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+        
+        for clave in data.items():
+            lista_json.append(list(clave))
+
+
+    return lista_json
+
 #Función encargada de cargar las dos listas principales: 1-Palabras 2-Definiciones
 def cargarListas(lista):
     '''Función que se va a encargar de llenar las N listas principales: - palabras = [] - definiciones1 = ["definición1"] - definiciones2 = ["definición2"] - definicionesN = ["definiciónN"]. 
-    En caso de solo llevar una definición, en su indice de la lista definiciones_2 se completará con un guión medio (-). Se retornrarán 3 listas con la palabras y las definiciones'''
+    En caso de solo llevar una definición, en su indice de la lista definiciones_2 se completará con un guión medio (-)'''
     
     
 
@@ -18,15 +29,16 @@ def cargarListas(lista):
 
     for i in range(len(lista)):
         palabras.append(list(lista[i][0]))
-        if len(lista[i]) > 2:
-            definiciones_1.append(lista[i][1])
-            definiciones_2.append(lista[i][2])
+        if len(lista[i][1]) > 1:
+            definiciones_1.append(lista[i][1][0])
+            definiciones_2.append(lista[i][1][1])
         else:
-            definiciones_1.append(lista[i][1])
+            definiciones_1.append(lista[i][1][0])
             definiciones_2.append("-")
 
-    return palabras,definiciones_1,definiciones_2
+   
 
+    return palabras,definiciones_1,definiciones_2
 def IngresarPalabraNumero():
     '''Función encargada del Ingreso de la palabra a adivinar siguiendo la lógica de número - palabra (1 - C A S A)
         Devuelve la palabra Ingresada, el número de la palabra a adivinar y si el usuario necesita una pista. '''
