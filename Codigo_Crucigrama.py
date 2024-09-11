12a3f52d88f4ca8077ecb58549c1ad4ae96b1d58
LucianoPerrella <129005908+LucianoPerrella@users.noreply.github.com>
2024-09-10 21:29:09 -0300

    Parche a funciones para primer entrega

--- Codigo_Crucigrama.py
+++ Codigo_Crucigrama.py
@@ -4,7 +4,7 @@ import json
 
 #Función encargada de leer json desde el archivo plano
 
-
+#No presentar, función a futuro para el uso de archivos
 def LeerJSON():
     lista_json = []
     with open("palabras.json", "r", encoding="utf-8") as file:
@@ -16,29 +16,43 @@ def LeerJSON():
 
     return lista_json
 
-#Función encargada de cargar las dos listas principales: 1-Palabras 2-Definiciones
-def cargarListas(lista):
-    '''Función que se va a encargar de llenar las N listas principales: - palabras = [] - definiciones1 = ["definición1"] - definiciones2 = ["definición2"] - definicionesN = ["definiciónN"]. 
-    En caso de solo llevar una definición, en su indice de la lista definiciones_2 se completará con un guión medio (-)'''
-    
-    
+#Para la primer entrega, usaremos un diccionario y cargaremos los datos desde ahi de manera representativa
 
+#Función encargada de reunir palabras y definiciones multiples (pueden variar por palabra) y las reune en una sublista de una lista
+def LeerDict(diccionario):
+    lista_extraida = []
+    for clave,valor in diccionario.items():
+        lista_extraida.append([clave,valor])
+    return lista_extraida
+
+'''Función encargada de cargar 4 listas: 1 de palabras y las siguientes para sus respectivas definiciones. 
+En caso de no contar con tantas definiciones, su indice en la lista correspondiente se completa con un "-" '''
+         
+def cargarListas(lista):
     palabras = []
     definiciones_1 = []
     definiciones_2 = []
-
+    definiciones_3 = []
+    
     for i in range(len(lista)):
-        palabras.append(list(lista[i][0]))
-        if len(lista[i][1]) > 1:
+        palabras.append(lista[i][0])
+        cantidad_definiciones = len(lista[i][1])
+        if cantidad_definiciones > 2:
             definiciones_1.append(lista[i][1][0])
             definiciones_2.append(lista[i][1][1])
+            definiciones_3.append(lista[i][1][2])
+        elif cantidad_definiciones > 1:
+            definiciones_1.append(lista[i][1][0])
+            definiciones_2.append(lista[i][1][1])
+            definiciones_3.append("-")
         else:
             definiciones_1.append(lista[i][1][0])
             definiciones_2.append("-")
-
-   
-
-    return palabras,definiciones_1,definiciones_2
+            definiciones_3.append("-")
+            
+            
+        
+    return palabras,definiciones_1,definiciones_2,definiciones_3
 def IngresarPalabraNumero():
     '''Función encargada del Ingreso de la palabra a adivinar siguiendo la lógica de número - palabra (1 - C A S A)
         Devuelve la palabra Ingresada, el número de la palabra a adivinar y si el usuario necesita una pista. '''
@@ -91,3 +105,26 @@ def BuscoCoincidencias(palabras):
 
 #MAIN
 IngresarPalabraNumero()
+diccionario_maqueta = {
+  "perro": [
+    "Mamífero doméstico de la familia de los cánidos, de tamaño, forma y pelaje muy diversos, según las razas, que tiene olfato muy fino y es inteligente y muy leal a su dueño. Usado en masculino referido a la especie.",
+    "El mejor amigo del hombre",
+    "Un capo sinceramente"
+  ],
+  "gato": [
+    "Mamífero de la familia de los félidos, digitígrado, doméstico, de unos 50 centímetro(s) de largo desde la cabeza hasta el arranque de la cola, que por sí sola mide unos 20 centímetro(s), de cabeza redonda, lengua muy áspera, patas cortas y generalmente pelaje suave y espeso, de color blanco, gris, pardo, rojizo o negro, empleado en algunos lugares para cazar ratones. Usado en masculino referido a la especie.",
+    "Minino"
+  ],
+  "banana": [
+    "Fruta tropical procedente de la planta herbácea que recibe el mismo nombre o banano, perteneciente a la familia de las musáceas. Tiene forma alargada o ligeramente curvada"
+  ],
+  "pelota": [
+    "Bola hecha de una materia que le permita botar, usada en diversos juegos y deportes.",
+    "esférico"
+  ]
+}
+
+lista = LeerDict(diccionario)
+palabras,def_01,def_02,def_03 = cargarListas(lista)
+
+
